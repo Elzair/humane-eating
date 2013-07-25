e63469b9b0efbad7dfbc018b9613a4f73b205046
Elzair <elzairthesorcerer@gmail.com>
2013-07-25 19:44:44 -0400

    Added script to call to run server

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+styldir='public/stylesheets'
+temps=''
+fluidity='fluidity.styl'
+imports=''
+# Get list of all templates that need watching
+cd $styldir
+for i in $(ls *.styl)
+do
+  if [[ $i != $fluidity ]]
+  then
+    temps=$temps' '$styldir'/'$i
+  fi
+done
+# Get list of all templates that need to be imported
+for i in $(cat fluidity.styl | awk {'print $2'} | sed s/\"//g)
+do
+  imports=$imports' --import '$styldir'/'$i
+done
+# Compile CSS templates and run app with nodemon
+cd ../../
+options=$temps$imports
+stylus $options & nodemon app.js
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 npm install
 cp ./node_modules/fluidity/lib/fluidity.styl ./public/stylesheets
 cp -R ./node_modules/fluidity/lib/fluidity ./public/stylesheets/
