529be92463466b9adf007df64ae6093d960a6048
Elzair <elzairthesorcerer@gmail.com>
2013-08-07 15:58:42 -0400

    Removed executable scripts from project

--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-npm install
-cp ./node_modules/fluidity/lib/fluidity.styl ./public/stylesheets
-cp -R ./node_modules/fluidity/lib/fluidity ./public/stylesheets/
-ender build jeesh reqwest
